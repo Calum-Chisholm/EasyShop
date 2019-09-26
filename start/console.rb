ebd09cc30f34a2f42b49c5975a99e10a4d8a193c
Calum Chisholm <calum.chisholm@hotmail.com>
2019-09-26 22:23:27 +0100

    added data to database

--- start/console.rb
+++ start/console.rb
@@ -13,24 +13,68 @@ new_product1 = {
 'name' => 'TV'
 }
 
+new_product2 = {
+  'name' => 'FroYo'
+}
+new_product3 = {
+  'name' => 'iPhone'
+}
+
 product1 = Product.new(new_product1)
 product1.save
+product2 = Product.new(new_product2)
+product2.save
+product3 = Product.new(new_product3)
+product3.save
 
 new_merchant1 = {
   "name" => "Tesco"
 }
+new_merchant2 = {
+  'name' => 'FoodCo'
+}
+new_merchant3 = {
+  'name' => 'Asda'
+}
 
 merchant1 = Merchant.new(new_merchant1)
 merchant1.save
+merchant2 = Merchant.new(new_merchant2)
+merchant2.save
+merchant3 = Merchant.new(new_merchant3)
+merchant3.save
 
 new_stock1 = {
   'product_id' => product1.id,
   'merchant_id' => merchant1.id,
   'price' => 500
 }
+new_stock2 = {
+  'product_id' => product2.id,
+  'merchant_id' => merchant2.id,
+  'price' => 10
+}
+new_stock3 = {
+  'product_id' => product3.id,
+  'merchant_id' => merchant3.id,
+  'price' => 800
+}
+new_stock4 = {
+  'product_id' => product1.id,
+  'merchant_id' => merchant3.id,
+  'price' => 2000
+}
 
 stock1 = Stock.new(new_stock1)
 stock1.save
+stock2 = Stock.new(new_stock2)
+stock2.save
+stock3 = Stock.new(new_stock3)
+stock3.save
+stock4 = Stock.new(new_stock4)
+stock4.save
+
+
 
 new_customer = {
   'name' => 'Ricky',
